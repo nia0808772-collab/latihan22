4c0147096ed1caeba42c7f48bb0a206214afa442
NiaNurulCahya <nia0808772@gmail.com>
2026-04-07 09:54:30 +0700

    update

--- main.js
+++ main.js
@@ -69,3 +69,21 @@ function edit() {
  document.getElementById("nomor").value =""
  document.getElementById("hargaedit").value =""
 }
+
+//fungsi untuk menghapus barang berdasarkan nomor barang
+function hapusNomor() {
+  //ubah nomor menjadi menjadi index array dengan mengurangi 1
+  let index = Number(document.getElementById("nomorHapus").value) -1
+  //periksa apakah index valid
+ if (index >= 0 && index < dataBarang.length) {
+   
+ dataBarang.splice(index, 1)
+ } else {
+   //jika nomor barang tidak valid, tampilan alert
+   alert("Nomor barang tidak valid")
+}
+
+tampilkan()
+//kosongkan input nomor hapus setelah di hapus
+document.getElementById("nomorHapus").value =""
+}
